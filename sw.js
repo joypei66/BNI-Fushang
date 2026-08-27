004d92dd18e6a56efdf209f1a558fa0981074250
joypei66 <joypei66@gmail.com>
2026-08-27 17:26:52 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='fushang-v1'; const ASSETS=['./','./index.html','./logo.jpg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
